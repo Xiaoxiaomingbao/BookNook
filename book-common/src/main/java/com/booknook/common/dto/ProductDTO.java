712bd97c39a830158b024d608694d77a1f55aa8d
Zhiming <xiaoxiaomingbao@gmail.com>
2025-03-20 14:32:46 +0800

    add three new modules

---
+++ book-common/src/main/java/com/booknook/common/dto/ProductDTO.java
@@ -0,0 +1,39 @@
+package com.booknook.common.dto;
+
+import com.booknook.common.enums.ProductCondition;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.Year;
+
+@Data
+@ApiModel(description = "商品实体")
+public class ProductDTO {
+    @ApiModelProperty("商品id")
+    private Long pid;
+    @ApiModelProperty("卖家id")
+    private Long uid;
+    @ApiModelProperty("商品名称")
+    private String name;
+    @ApiModelProperty("ISBN(不带横杠)")
+    private String isbn;
+    @ApiModelProperty("出版社")
+    private String publisher;
+    @ApiModelProperty("出版时间")
+    private Year publishTime;
+    @ApiModelProperty("作者")
+    private String author;
+    @ApiModelProperty("类目名称")
+    private String category;
+    @ApiModelProperty("商品描述")
+    private String description;
+    @ApiModelProperty("商品成色")
+    private ProductCondition condition;
+    @ApiModelProperty("商品状态 1-正常，2-下架，3-删除")
+    private Integer status;
+    @ApiModelProperty("价格（分）")
+    private Integer price;
+    @ApiModelProperty("库存数量")
+    private Integer stock;
+}
---
+++ book-common/src/main/java/com/booknook/common/enums/ProductCondition.java
@@ -0,0 +1,28 @@
+package com.booknook.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ProductCondition {
+
+    NEW(1, "全新"),
+    LIKE_NEW(2, "九成新"),
+    GOOD(3, "七成新"),
+    ACCEPTABLE(4, "五成新");
+
+    private final int value;
+
+    private final String desc;
+
+    ProductCondition(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    public boolean equalsValue(Integer value) {
+        if (value == null) {
+            return false;
+        }
+        return getValue() == value;
+    }
+}
