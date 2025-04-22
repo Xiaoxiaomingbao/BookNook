5f0132db9213614ecab1053d3704305aa05d1d89
Zhiming <xiaoxiaomingbao@gmail.com>
2025-04-23 01:37:14 +0800

    book-gateway: 网关模块实现拦截（测试/users/1通过）和放行（测试/products/1通过）特定路径的请求，配置JWT工具

--- book-gateway/src/main/java/com/booknook/gateway/GatewayApplication.java
+++ book-gateway/src/main/java/com/booknook/gateway/GatewayApplication.java
@@ -1,11 +1,14 @@
 package com.booknook.gateway;
 
+import com.booknook.gateway.config.AuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(AuthProperties.class)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
